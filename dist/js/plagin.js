886b5fb17555855df0f1191044ef91ca8734be73
Sabbash <Sabbash23@Gmail.com>
2016-12-08 17:38:33 +0200

    Initial commit

---
+++ dist/js/plagin.js
@@ -0,0 +1,18 @@
+// scroll function
+var header = $('header');
+var windowHeight = $(window).height();
+
+$(window).on('scroll', function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > windowHeight / 2) {
+        header.addClass('fixed');
+    } else {
+        header.removeClass('fixed')
+    }
+})
+
+//slick nav mobile
+
+$(function(){
+    $('#menu').slicknav();
+});
