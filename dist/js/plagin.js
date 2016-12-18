c652ae6578f7375db50be180b826436ce1310a92
Sabbash <Sabbash23@Gmail.com>
2016-12-19 00:03:22 +0200

    123

--- dist/js/plagin.js
+++ dist/js/plagin.js
@@ -54,8 +54,39 @@
     })
 })(jQuery);
 
+//animation
+(function ($) {
+    $(window).on('load', function () {
+        $('body,html').animate({scrollTop: 0}, 100);
 
+        var animateElements = $('[data-animate]');
 
+        $('[data-animate="load"]').addClass('animate');
+
+        function isVisible(elem) {
+
+            var coordinates = elem.getBoundingClientRect();
+
+            var windowHeight = document.documentElement.clientHeight;
+
+            return coordinates.top > 0 && coordinates.top < windowHeight;
+        }
+
+        var windowWidth = $(window).width();
+
+        if (windowWidth >= 1024) {
+            $(window).on('scroll load', function () {
+                for (var i = 0; i < animateElements.length; i++) {
+                    if (isVisible(animateElements[i])) {
+                        $(animateElements[i]).addClass('animate')
+                    }
+                }
+            })
+        } else {
+            $('[data-animate="scroll"]').addClass('animate');
+        }
+    });
+})(jQuery);
 
 
 
